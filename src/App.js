203a0a988e1291f89d399c75bbaef553d0250f3a
Roni Kauppinen <n0kar00@students.oamk.fi>
2022-03-16 19:37:12 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,74 +1,76 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost/shoppinglist'
 
 function App() {
-  const [weight, setWeight] = useState(0);
-  const [bottle, setBottles] = useState(0);
-  const [time, setTime] = useState(0);
-  const [gender, setGender] = useState('male');
-  const [results, setResults] = useState(0);
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState('');
+  const [amount, setAmount] = useState();
 
-  function calculate(e) {
-    e.preventDefault();
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+      setItem(response.data)
+    }).catch(error => {
+      alert(error);
+    })
+}, [])
 
-    let results = 0;
-    let litres = bottle * 0.33;
-    let grams = litres * 8 * 4.5;
-    let burning = weight / 10;
-    let gramsLeft = grams - (burning * time);
-
-    if (gender === 'male') {
-      results = gramsLeft / (weight * 0.7);
-    }
-    else {
-      results = gramsLeft / (weight * 0.6);
-    }
-    
-    if (results < 0) {
-      results = 0;
-    }
-    setResults(results);
-  }
-
-  return (
-    <form onSubmit={calculate}>
-      <h3>Calculate alcohol blood level</h3>
-
-      <div>
-        <div>
-          <label>Weight: </label>
-          <input type="number" value={weight} onChange={e => setWeight(e.target.value)} 
-          />
-        </div>
-        <div>
-
-          <label>Bottles: </label>
-          <input type="number" value={bottle} onChange={e => setBottles(e.target.value)}/>
-
-        </div>
-        <div>
-
-          <label>Time: </label>
-          <input type="number" value={time} onChange={e => setTime(e.target.value)}/>
-
-        </div>
-        <p>Gender:</p>
-
-        <input type="radio" name="gender" value="male" defaultChecked 
-        onChange={e => setGender(e.target.value)}/>
-        <label>Male</label>
+function remove(id) {
+  const json = JSON.stringify({id:id})
+  axios.post(URL + 'delete.php', json,{
+   headers: {
+     'Content-Type' : 'application/json'
+   }
+  })
+  .then((response) => {
+    const newListWithRemoved = item.filter((item) => item.id !== id);
+    const amountListRemoved = amount.filter((amount) =>amount.id !== id)
+    setAmount(amountListRemoved);
+    setItem(newListWithRemoved);
+  }) .catch (error => {
+    alert(error.response ? error.response.data.error : error);
+  })
+} 
         
-        <input type="radio" name="gender" value="female"
-        onChange={e => setGender(e.target.value)} />
-        <label>Female</label>
+function save(e) {
+  e.preventDefault();
+  const json = JSON.stringify({desription:item},{desription:amount})
+  axios.post(URL + 'add.php', json, {
+    headers: {
+      'Content-Type' : 'application/json'
+    }
+  })
+  .then((response) => {
+    setItem(item => [...item,response.data]);
+    setItem('');
+  }).catch (error => {
+    alert(error.response.data.error)
+  });
+}
+         
+  return (
+    <div className='container'>
+      <h2>shoppinglist</h2>
+      <form onSubmit={save}>
+        <label>New Item</label>
+        <input value={item} onChange={e => setItem(e.target.value)}/>
 
-      </div>
-      <button>Calculate</button>
-      <div>
-        <output>{results.toFixed(2)}</output>
-      </div>
-      
-    </form>
+        <input value={amount} onChange={e => setAmount(e.target.value)}/>
+        <button>Add</button>
+      </form>
+      <ol>
+      {item?.map(item =>(
+          <li key={item.id}>{item.desription}
+          <a href='#'className='delete' onClick={() => remove(item.id,amount.id)}>
+            Delete 
+          </a>
+         </li>
+        ))}
+      </ol> 
+    </div>
   );
 }
 
